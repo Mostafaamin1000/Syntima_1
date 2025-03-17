bfed9841e2f28f8a1627ecf57e9346e11f18694a
abdelrahman arfat <abdoarfat924@gmail.com>
2025-03-17 11:43:46 +0200

    change schema to customize drag and drop question

--- DB/models/question.schema.js
+++ DB/models/question.schema.js
@@ -1,31 +1,31 @@
-import mongoose, { Schema, Types, model } from 'mongoose'
-
-
-const schema = new Schema ({
-    level:{type:Types.ObjectId, ref:"Level"},
-    sign_Url:String,
-    sign_Text:String, 
-    type: { // Question types
+import mongoose, { Schema, Types, model } from 'mongoose';
+const schema = new Schema({
+    level: { type: Types.ObjectId, ref: "Level" },
+    sign_Urls: { type: [String], default: [] }, // Allow multiple URLs
+    sign_Texts: { type: [String], default: [] }, // Allow multiple texts
+    type: {
         type: String,
         required: true,
-        enum: ['MCQ', 'T/F','drag&drop']},
-question:{type:String, required:true},
-options:[
-    {
-        text:String,
-        score:{type:Number, min:0, max:10, default:0}
+        enum: ['MCQ', 'T/F', 'drag_drop']
+    },
+    question: { type: String, required: true },
+    options: [
+        {
+            text: String,
+            score: { type: Number, min: 0, max: 10, default: 0 }
+        }
+    ],
+    correctOptions: { type: [String], default: [] } // Store multiple correct options
+}, {
+    timestamps: true,
+    versionKey: false
+});
+
+// Update URLs dynamically
+schema.post('init', function (doc) {
+    if (doc.sign_Urls.length > 0) {
+        doc.sign_Urls = doc.sign_Urls.map(url => process.env.BASE_URL + "questions/" + url);
     }
-],
-correctOption:{ type: mongoose.Schema.Types.Mixed}
-},{
-    timestamps:true,
-    versionKey:false
-})
+});
 
-schema.post('init',function(doc){
-    if(doc.sign_Url) doc.sign_Url = process.env.BASE_URL+"questions/" + doc.sign_Url
-})
-
-
-export const Question = model('Question',schema)
-
+export const Question = model('Question', schema);
--- src/modules/question/question.controller.js
+++ src/modules/question/question.controller.js
@@ -1,44 +1,50 @@
 import { Question } from "../../../DB/models/question.schema.js";
 import { catchError } from "../../middlewares/catchError.js";
 
+// Add Question (Now supports multiple sign images)
+const AddQuestions = catchError(async (req, res, next) => {
+    if (req.files && req.files.sign_Url) {
+        req.body.sign_Urls = req.files.sign_Url.map(file => file.filename);
+    }
+
+    let question = await Question.create(req.body);
+    res.status(200).json({ message: "Created successfully!", question });
+});
 
 
-const AddQuestions =catchError(async(req,res,next)=>{
- req.body.sign_Url=req.file.filename
-let question = await Question.insertMany(req.body)
-res.status(200).json({message:"Created .. ", question})
-})
 
+// Get All Questions
+const getAllQuestions = catchError(async (req, res, next) => {
+    let questions = await Question.find();
+    res.status(200).json({ message: "All questions retrieved", questions });
+});
 
-const getAllQuestions =catchError(async(req,res,next)=>{
-    let questions = await Question.find()
-    res.status(200).json({message:"all .. ", questions})
-    })
+// Get One Question by ID
+const getOneQuestion = catchError(async (req, res, next) => {
+    let question = await Question.findById(req.params.id);
+    res.status(200).json({ message: "Question retrieved", question });
+});
 
+// Update Question
+const updateQuestion = catchError(async (req, res, next) => {
+    if (req.files && req.files.sign_Urls) {
+        req.body.sign_Urls = req.files.sign_Urls.map(file => file.filename);
+    }
 
-    const getoneQuestions =catchError(async(req,res,next)=>{
-        let question = await Question.findById(req.params.id)
-        res.status(200).json({message:"all .. ", question})
-        })
+    let question = await Question.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json({ message: "Updated successfully", question });
+});
 
-
-    const updateQuestions =catchError(async(req,res,next)=>{
-         if(req.body.sign_Url)req.body.sign_Url=req.file.filename
-        let question = await Question.findByIdAndUpdate(req.params.id,req.body,{new:true})
-        res.status(200).json({message:"all .. ", question})
-        })
-
-
-    const deleteQuestions =catchError(async(req,res,next)=>{
-        let questions = await Question.findByIdAndDelete(req.params.id)
-        res.status(200).json({message:"all .. ", questions})
-        })
-    
+// Delete Question
+const deleteQuestion = catchError(async (req, res, next) => {
+    let question = await Question.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Deleted successfully", question });
+});
 
 export {
     AddQuestions,
     getAllQuestions,
-    deleteQuestions,
-    updateQuestions,
-    getoneQuestions
-}
+    getOneQuestion,
+    updateQuestion,
+    deleteQuestion
+};

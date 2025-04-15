275763690a684f17e580439583dc77cf8de7f710
Abd44Arfat <abdoarfat924@gmail.com>
2025-04-15 15:49:17 +0200

    ..

--- DB/models/question.schema.js
+++ DB/models/question.schema.js
@@ -1,11 +1,9 @@
-import mongoose, { Schema, Types, model } from 'mongoose'
-
-
-const schema = new Schema ({
-    level:{type:Types.ObjectId, ref:"Level"},
-    sign_Url:String,
-    sign_Text:String, 
-    type: { // Question types
+import mongoose, { Schema, Types, model } from 'mongoose';
+const schema = new Schema({
+    level: { type: Types.ObjectId, ref: "Level" },
+    sign_Urls: { type: [String], default: [] }, // Allow multiple URLs
+    sign_Texts: { type: [String], default: [] }, // Allow multiple texts
+    type: {
         type: String,
         required: true,
         enum: ['MCQ', 'T/F', 'drag_drop']
@@ -22,6 +20,16 @@ const schema = new Schema ({
     timestamps: true,
     versionKey: false
 });
+
+
+schema.pre('save', function (next) {
+    // Filter options where score is 10 (or your threshold for "correct")
+    this.correctOptions = this.options
+      .filter(option => option.score === 10) // Adjust this condition based on your logic
+      .map(option => option.text); // Store the text of correct options
+    next();
+  });
+
 
 // Update URLs dynamically
 schema.post('init', function (doc) {

7e15800e1b492a87b1daf10d9bd3bd0b5814cc30
Mostafaamin1000 <mostafaamin09098@mail.com>
2025-05-12 08:38:01 -0700

    Add vercel config

--- DB/db.connection.js
+++ DB/db.connection.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose'
-
-export const dbConnection =mongoose.connect('mongodb+srv://abdelrahman:06925270@cluster0.6line1m.mongodb.net/').then(()=>{
+import dotenv from 'dotenv'
+dotenv.config()
+export const dbConnection =mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Database Connected Successfully ..");
 }).catch(()=>{
     console.log("Error in Connection DB ..");
     
 })
 
-//! mongodb+srv://abdelrahman:06925270@cluster0.6line1m.mongodb.net/
-// mongodb://127.0.0.1:27017/gproject
+

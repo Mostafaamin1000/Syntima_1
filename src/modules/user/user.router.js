6f6386d595a4cbcaf5b9f3256a52229764bceab7
Mostafaamin1000 <mostafaamin09098@mail.com>
2025-06-02 06:14:58 -0700

    try somthing

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -7,7 +7,7 @@ const userRouter=Router()
 
 userRouter.get('/',getallUsers)
 userRouter.get('/:id',getUser)
-userRouter.patch('/uploadImage',protectedRouter,allowTo('user','admin'),uploadSinleFile('image','user'),uploadProfilePic)
+// userRouter.patch('/uploadImage',protectedRouter,allowTo('user','admin'),uploadSinleFile('image','user'),uploadProfilePic)
 userRouter.put('/:id',uploadSinleFile('image','user'),updateUser)
 userRouter.delete('/:id',deleteUser)
 
